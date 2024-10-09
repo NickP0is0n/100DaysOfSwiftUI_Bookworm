738d69af3690a2357d12b84ded2c1c930bab57b1
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-10-09 16:14:26 +0300

    List of books

---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Mykola Chaikovskyi on 09.10.2024.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("😞")
+        case 2:
+            Text("🥱")
+        case 3:
+            Text("😐")
+        case 4:
+            Text("☺️")
+        default:
+            Text("🤩")
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)
+}
